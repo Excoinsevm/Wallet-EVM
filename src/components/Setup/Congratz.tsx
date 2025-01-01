199e57113f86f36a5374353bd878b2d1b4b39466
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2025-01-01 06:07:29 +0000

    Update Congratz.tsx

--- src/components/Setup/Congratz.tsx
+++ src/components/Setup/Congratz.tsx
@@ -6,15 +6,15 @@ export const Congratz = () => {
         <>
         <div className="grid justify-items-center gap-3 p-6">
             <div className="flex">
-                <div className="text-3xl font-sans font-extrabold text-orange-500">Felicidades</div>
+                <div className="text-3xl font-sans font-extrabold text-orange-500">Congratulations</div>
                 <Confetti className="rounded-lg w-full h-full"/>
             </div>
             <div className="flex">
-                <div className="font-bold text-base text-gray-800">Has creado correctamente tu billetera</div>
+                <div className="font-bold text-base text-gray-800">Your wallet was created</div>
             </div>
         </div>
 
-        <Button as={Link} href="/" className="btn-gradient text-white">Continuar</Button>
+        <Button as={Link} href="/" className="btn-gradient text-white">Continue</Button>
         </>
     )
 }
